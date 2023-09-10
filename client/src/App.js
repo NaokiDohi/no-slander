8054e4315d1ec22e0f243606b25c2257794c9ec7
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2023-09-10 10:19:55 +0900

    Add Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Button } from '@web3uikit/core'
+import { Twitter, Metamask } from '@web3uikit/icons'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Settings from './pages/Settings'
@@ -8,22 +10,38 @@ import Rightbar from './components/Rightbar'
 import './App.css'
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
   return (
-    <div className='page'>
-      <div className='sideBar'>
-        <Sidebar />
-      </div>
-      <div className='mainWindow'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/settings' element={<Settings />} />
-        </Routes>
-      </div>
-      <div className='rightBar'>
-        <Rightbar />
-      </div>
-    </div>
+    <>
+      {isAuthenticated ? (
+        <div className='page'>
+          <div className='sideBar'>
+            <Sidebar />
+          </div>
+          <div className='mainWindow'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/settings' element={<Settings />} />
+            </Routes>
+          </div>
+          <div className='rightBar'>
+            <Rightbar />
+          </div>
+        </div>
+      ) : (
+        <div className='loginPage'>
+          <Twitter fill='#ffffff' fontSize={80} />
+          <Button
+            onClick={null}
+            size='xl'
+            text='Login with Metamask'
+            theme='primary'
+            icon={<Metamask />}
+          />
+        </div>
+      )}
+    </>
   )
 }
 
