e9fb0ccaab2e1c7b863e1ee8a85fef7ea57f17f0
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2023-09-10 09:56:44 +0900

    Add Profile page.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,19 +1,26 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
+import { Matic } from '@web3uikit/icons'
+import TweetInFeed from '../components/TweetInFeed'
 import './Profile.css'
+import { defaultImgs } from '../defaultImgs'
 
 const Profile = () => {
   return (
     <>
-      <Link to={'/'}>
-        <div>Home</div>
-      </Link>
-      <Link to={'/profile'}>
-        <div>Profile</div>
-      </Link>
-      <Link to={'/settings'}>
-        <div>Settings</div>
-      </Link>
+      <img className='profileBanner' src={defaultImgs[1]} />
+      <div className='pfpContainer'>
+        <img className='profilePFP' src={defaultImgs[0]} />
+        <div className='profileName'>Naoki</div>
+        <div className='profileWallet'>0x3172368721637826378163854123</div>
+        <Link to='/settings'>
+          <div className='profileEdit'>Edit Profile</div>
+        </Link>
+        <div className='profileBio'>Full stack engineer</div>
+        <div className='profileTabs'>
+          <div className='profileTab'>Your Tweets</div>
+        </div>
+      </div>
+      <TweetInFeed profile={true}></TweetInFeed>
     </>
   )
 }
